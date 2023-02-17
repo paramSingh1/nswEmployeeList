813cb274f6aec79a71bde1ef3a171ab723838042
paramSingh1 <paramdeep2009@hotmail.com>
2023-02-17 19:44:48 +1100

    Created employee Entity

---
+++ backend/employeelist/src/main/java/Employee/Employee.java
@@ -0,0 +1,168 @@
+package Employee;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Long id;
+
+	@Column
+	private String firstName;
+
+	@Column
+	private String middleName;
+
+	@Column
+	private String lastName;
+
+	@Column
+	private String email;
+
+	@Column
+	private String mobileNumber;
+
+	@Column
+	private String resAddress;
+
+	@Column
+	private String contractType;
+
+	@Column
+	private String startDate;
+
+	@Column
+	private String endDate;
+
+	@Column
+	private String timeBasis;
+
+	@Column
+	private Integer weeklyHours;
+
+	// Empty Constructor to mitigate Spring's Default constructor not found error
+	public Employee() {
+	}
+
+	// Constructor
+	public Employee(String firstName, String middleName, String lastName, String email, String mobileNumber,
+			String resAddress, String contractType, String startDate, String endDate, String timeBasis,
+			Integer weeklyHours) {
+		this.firstName = firstName;
+		this.middleName = middleName;
+		this.lastName = lastName;
+		this.email = email;
+		this.mobileNumber = mobileNumber;
+		this.resAddress = resAddress;
+		this.contractType = contractType;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.timeBasis = timeBasis;
+		this.weeklyHours = weeklyHours;
+	}
+
+	// Getters and setters to access private fields for additional security - (Encapsulation)
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getMiddleName() {
+		return middleName;
+	}
+
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+
+	public String getResAddress() {
+		return resAddress;
+	}
+
+	public void setResAddress(String resAddress) {
+		this.resAddress = resAddress;
+	}
+
+	public String getContractType() {
+		return contractType;
+	}
+
+	public void setContractType(String contractType) {
+		this.contractType = contractType;
+	}
+
+	public String getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+
+	public String getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+
+	public String getTimeBasis() {
+		return timeBasis;
+	}
+
+	public void setTimeBasis(String timeBasis) {
+		this.timeBasis = timeBasis;
+	}
+
+	public Integer getWeeklyHours() {
+		return weeklyHours;
+	}
+
+	public void setWeeklyHours(Integer weeklyHours) {
+		this.weeklyHours = weeklyHours;
+	}
+
+}
